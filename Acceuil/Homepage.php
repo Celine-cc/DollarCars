51e307f36a575aa4c0cabb8691539e0a9947ff9f
Lazarone <leabarisani@gmail.com>
2023-06-15 10:10:05 +0200

    first commit

---
+++ Acceuil/Homepage.php
@@ -0,0 +1,19 @@
+<?php
+
+//namespace
+
+use PDO;
+
+class Enchere
+
+{
+    protected $nom;
+    protected $prenom;
+    protected $typevoiture;
+    protected $marque;
+    protected $puissance;
+    protected $annee;
+    protected $kilometrage;
+    protected $carburent;
+    protected $description;
+}
---
+++ Acceuil/Index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Acceuil</title>
+</head>
+
+<body>
+    <p class="entete">
+    <h1><strong><i>Dollar Cars $</i></strong></h1>
+    <p>Site d'enchères en ligne. Vendez ici votre voiture à prix gagnant !
+        <br>Dollar Cars le premier site d'annonce en ligne de la region dollar.
+    </p>
+    </p>
+
+    <p><strong>Déposez ici votre annonce
+        </strong>
+    <form action="annonce">
+        <input type="button" value="">
+        <input type="button" value="">
+        <input type="button" value="">
+        <input type="button" value="">
+        <input type="button" value="">
+        <input type="button" value="">
+        <input type="button" value="">
+        <input type="button" value="">
+        <input type="button" value="">
+
+
+    </form>
+
+    </p>
+
+</body>
+
+</html>
