e62b3054afa4a683a980e12b12249803264104d3
TheoCRLDS <theo.carladous@laposte.net>
2023-06-15 14:06:20 +0200

    structure v2

--- Models/Cars.php
+++ Models/Cars.php
@@ -1,6 +1,7 @@
 <?php
 
-//namespace
+namespace Models;
+
 include_once __DIR__ . "\DollarCars\Accueil\indexHome.php";
 
 use PDO;
@@ -9,49 +10,37 @@ class Annonce
 
 {
     protected $id;
-    protected $nom;
-    protected $prenom;
-    protected $mail;
     protected $typeVoiture;
     protected $marque;
     protected $puissance;
     protected $annee;
-    protected $kilometrage;
-    protected $carburant;
     protected $description;
+    protected $prix;
 
 
     public function __construct(
         $id,
-        $nom,
-        $prenom,
-        $mail,
         $typeVoiture,
         $marque,
         $puissance,
         $annee,
-        $kilometrage,
-        $carburant,
-        $description
+        $description,
+        $prix
     ) {
 
         $this->id = $id;
-        $this->nom = $nom;
-        $this->prenom = $prenom;
-        $this->mail = $mail;
         $this->typeVoiture = $typeVoiture;
         $this->marque = $marque;
         $this->puissance = $puissance;
         $this->annee = $annee;
-        $this->kilometrage = $kilometrage;
-        $this->carburant = $carburant;
         $this->description = $description;
+        $this->prix = $prix;
     }
     public function annonce()
     {
     }
 
     public function __get($donnees)
-    { if 
+    {
     }
 }
