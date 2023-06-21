134cefc0c466801a920b3fc62f1fd84903e81882
Celine CC <celine.c11@outlook.com>
2023-06-21 09:13:41 +0200

    creationModifProfil

--- Models/Database.php
+++ Models/Database.php
@@ -20,9 +20,10 @@ use PDO;
 abstract class Database
 {
 
-    const ADDRESS = "mysql:dbname=dollarcars;host=127.0.0.1";
+    const ADDRESS = "mysql:dbname=dollarcars;host=127.0.0.1;port=8889
+    ";
     const USER = "root";
-    const PASSWORD = "";
+    const PASSWORD = "root";
 
     /**
      * Création d'un connexion à la base de données
